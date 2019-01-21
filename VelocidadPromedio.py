e11c401a06cbfbe02e4f75e44285c9f72c1d1190
PaulinaGuerrero <46898127+PaulinaGuerrero@users.noreply.github.com>
2019-01-21 09:40:21 -0600

    Paulina Guerrero Ruiz, Grupo 02,

---
+++ VelocidadPromedio.py
@@ -0,0 +1,9 @@
+# Author: Paulina Guerrero Ruiz
+# Calcula la velocidad promedio en un viaje
+
+tiempo = int(input("Teclea el tiempo del viaje en hrs. enteras: "))
+distancia = int(input("Teclea la distancia del viaje en kilometros enteros: "))
+
+velocidad = distancia / tiempo
+
+print("La velocidad promedio es:",velocidad, "km/h")
